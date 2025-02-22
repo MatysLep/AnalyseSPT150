5833a013bb4e52fe9f01bb06184a15fe4ff1cad4
MatysLep <144113894+MatysLep@users.noreply.github.com>
2025-02-22 19:18:05 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,88 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime
+
+
+measure_units = {0:"mg/m3" , 1:"g/m3", 2:"µg/m3", 3:"%"}
+
+
+
+st.title("Analyse du SPT150")
+uploaded_file = st.file_uploader("Choisir un fichier csv", type="csv")
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file, encoding="UTF-16", delimiter="\t", skiprows=1)
+    unit = measure_units[df['Unité de mesure'].iloc[0]]
+
+    df['Datetime'] = pd.to_datetime(df['Date'] + ' ' + df['Heure'], dayfirst=True)
+    df.set_index('Datetime', inplace=True)
+    df.index = df.index.strftime('%d/%m/%Y %H:%M:%S')
+    df['DateTimeFormat'] = pd.to_datetime(df['Date'] + ' ' + df['Heure'], dayfirst=True)
+
+    df['Heure'] = pd.to_datetime(df['Heure'], format='%H:%M:%S').dt.time
+
+    df['DateFormat'] = pd.to_datetime(df['Date'])
+    df['Date'] = pd.to_datetime(df['Date'])
+    df['Date'] = df['Date'].dt.strftime('%d/%m/%Y')
+
+    # Variable
+
+    col_to_display = ['Date', 'Heure','Mesure instantannée','Mesure moyennée sur 1 min.','Mesure moyennée sur X min.','Facteur de calibration']
+
+
+
+    st.divider()
+
+    st.write(f"Analyse des données disponible entre **{df.index[0]}** et **{df.index[-1]}**.")
+    col1,col2 = st.columns(2)
+    start_date = col1.date_input("Date de Début", value=df["DateFormat"].iloc[0], min_value=df["DateFormat"].iloc[0], max_value=df["DateFormat"].iloc[-1], format="DD/MM/YYYY")
+    start_time = col1.time_input("Heure de début", value="00:00")
+    end_date = col2.date_input("Date de Fin", value=df["DateFormat"].iloc[-1], min_value=start_date, max_value=df["DateFormat"].iloc[-1], format="DD/MM/YYYY")
+    end_time = col2.time_input("Heure de Fin", value="23:59")
+
+    colSeuil1, colSeuil2 = st.columns(2)
+    with_seuil = colSeuil1.toggle("Avec valeur de seuil")
+
+
+
+    start_datetime = datetime.combine(start_date, start_time)
+    end_datetime = datetime.combine(end_date, end_time)
+    df_filtered = df[(df['DateTimeFormat'] >= start_datetime) & (df['DateTimeFormat'] <= end_datetime)]
+
+    st.divider()
+
+    st.write("## Visualisation des données")
+    st.dataframe(df_filtered[col_to_display])
+
+    st.divider()
+
+    moyenne_value = df_filtered['Mesure instantannée'].mean().round(2)
+    min_value = df_filtered['Mesure instantannée'].min()
+    max_value = df_filtered['Mesure instantannée'].max()
+
+    if with_seuil:
+        num_seuil = colSeuil2.number_input("Valeur du seuil ")
+        nb_seuil_depasse = df_filtered[df_filtered['Mesure instantannée'] > num_seuil]['Mesure instantannée'].count()
+
+
+    # Partie sur les statistiques
+
+    st.write("## Statistiques")
+    col3,col4,col5 = st.columns(3)
+    col3.write(f"**Moyenne :** {moyenne_value} {unit}")
+    col4.write(f"**Min :** {min_value} {unit}")
+    col5.write(f"**Max :** {max_value} {unit}")
+
+    if with_seuil:
+        st.write(f"**Nombre de fois où le seuil est dépassé :** {nb_seuil_depasse}")
+
+    st.divider()
+
+    # Partie sur les graphes
+
+    chart_data = df_filtered[[
+        'Mesure instantannée',
+        'Mesure moyennée sur 1 min.',
+        'Mesure moyennée sur X min.'
+    ]]
+    st.line_chart(data=chart_data.tail(10000))
